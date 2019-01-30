d4fcbe514ebbf055549a3738289b3abf04cbb3ce
Alexandre <alebarbieri09@gmail.com>
2019-01-29 23:13:02 -0200

    First Commit

---
+++ src/main/java/com/spring/course/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,40 @@
+package com.spring.course.restfulwebservices.helloworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	{
+		System.out.println("HelloWorldController: Instância criada");
+	}
+	
+	@Autowired
+	private MessageSource messageSource;
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	// @RequestHeader(name="Accept-Language", required=false) Locale locale
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
+}
