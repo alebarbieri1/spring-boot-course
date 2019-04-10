49ab89a21b45660de87f69880ce6e5d1760083ff
Alexandre Barbieri <Alexandre.Barbieri@dieboldnixdorf.com>
2019-04-10 00:42:03 -0300

    JPA

---
+++ src/main/java/com/spring/course/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,20 @@
+package com.spring.course.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+	@GetMapping(value = "person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(value = "person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values (10001, sysdate(), 'Ale');
+insert into user values (10002, sysdate(), 'Gui');
+insert into user values (10003, sysdate(), 'Ma');
+
+insert into post values (10001, 'My First post', 10001);
+insert into post values (10002, 'My Second post', 10002);
